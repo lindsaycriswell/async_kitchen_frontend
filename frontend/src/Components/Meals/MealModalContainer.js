9e88dbe162680af8ee75e079dbde19cf048d046e
lindsaycriswell <lindsaycriswell@gmail.com>
2018-03-19 21:24:16 -0400

    meal modal works

--- frontend/src/Components/Meals/MealModalContainer.js
+++ frontend/src/Components/Meals/MealModalContainer.js
@@ -4,7 +4,7 @@ import RecipeCard from "../Recipes/RecipeCard";
 
 class MealModalContainer extends React.Component {
   state = {
-    mealTime: {}
+    mealTime: []
   };
 
   handleChange = e => {
@@ -13,15 +13,29 @@ class MealModalContainer extends React.Component {
     });
   };
 
+  // handleClick = e => {
+  //   e.preventDefault();
+  //   console.log("clicked");
+  // };
+
+  // figure out props
   render() {
     console.log(this.props.recipes);
     return (
       <div className="sixteen wide column">
+        <h3 style={{ color: "white" }}>What time would you like to eat?</h3>
+        <input
+          type="time"
+          onChange={this.handleChange}
+          label="What time would you like to eat?"
+        />
+        <div />
+
         <Modal
           style={{ marginLeft: "480px", marginTop: "90px" }}
           trigger={
             <Button
-              size="massive"
+              size="medium"
               style={{
                 marginTop: "10px",
                 marginBottom: "40px"
@@ -39,13 +53,11 @@ class MealModalContainer extends React.Component {
             ))}
             <Modal.Description>
               {this.props.recipes.map(recipe => (
-                <MealModalContainer recipe={recipe} key={recipe.id} />
+                <RecipeCard recipe={recipe} key={recipe.id} />
               ))}
             </Modal.Description>
           </Modal.Content>
         </Modal>
-
-        <input type="time" onChange={this.handleChange} />
       </div>
     );
   }
