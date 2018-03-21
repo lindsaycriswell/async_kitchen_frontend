e42f779f37fd67917c1d4e799eb68ad75fc37222
github username <github email address>
2018-03-21 11:49:08 -0400

    dispatch filters

--- frontend/src/Components/Filters.js
+++ frontend/src/Components/Filters.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { Input, Dropdown } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { addFilter } from "../actions/filters";
 
-const Filters = props => {
-  function dedupe(array) {
+class Filters extends React.Component {
+  state = {
+    search: "",
+    course: "",
+    ingredients: []
+  };
+
+  dedupe(array) {
     let newArray = [];
     for (var i = 0; i < array.length; i++) {
       if (array.indexOf(array[i]) === i) {
@@ -13,7 +21,6 @@ const Filters = props => {
         });
       }
     }
-    console.log(newArray);
     return newArray.sort(function(a, b) {
       let textA = a.text.toUpperCase();
       let textB = b.text.toUpperCase();
@@ -28,37 +35,65 @@ const Filters = props => {
     });
   }
 
-  const allCourses = props.recipes.map(recipe => recipe.course);
-  const uniqueCourses = dedupe(allCourses);
+  handleChange = (e, data) => {
+    if (e.target.name) {
+      this.setState({
+        search: e.target.value
+      });
+    } else if (data.name === "course") {
+      this.setState({
+        course: data.value
+      });
+    } else if (data.name === "ingredients") {
+      this.setState({
+        ingredients: data.value
+      });
+    }
+    this.props.addFilter(this.state);
+  };
 
-  const allIngredients = props.ingredients.map(
-    ingredient => ingredient.search_name
-  );
-  const uniqueIngredients = dedupe(allIngredients);
+  render() {
+    const allCourses = this.props.recipes.map(recipe => recipe.course);
+    const uniqueCourses = this.dedupe(allCourses);
 
-  return (
-    <div style={{ marginBottom: "20px" }}>
-      <Input icon="search" placeholder="Search Recipes..." />
-      <div style={{ margin: "15px" }} />
-      <Dropdown
-        style={{ width: "14vw", marginRight: "10px" }}
-        placeholder="Type of course"
-        compact
-        search
-        selection
-        options={uniqueCourses}
-      />
-      <Dropdown
-        style={{ width: "14vw", marginLeft: "10px" }}
-        placeholder="Filter by ingredient"
-        compact
-        multiple
-        search
-        selection
-        options={uniqueIngredients}
-      />
-    </div>
-  );
-};
+    const allIngredients = this.props.ingredients.map(
+      ingredient => ingredient.search_name
+    );
+    const uniqueIngredients = this.dedupe(allIngredients);
 
-export default Filters;
+    return (
+      <div style={{ marginBottom: "20px" }}>
+        <Input
+          icon="search"
+          placeholder="Search Recipes..."
+          name="search"
+          onChange={this.handleChange}
+        />
+        <div style={{ margin: "15px" }} />
+        <Dropdown
+          style={{ width: "14vw", marginRight: "10px" }}
+          placeholder="Type of course"
+          name="course"
+          compact
+          search
+          selection
+          options={uniqueCourses}
+          onChange={this.handleChange}
+        />
+        <Dropdown
+          style={{ width: "14vw", marginLeft: "10px" }}
+          placeholder="Filter by ingredient"
+          name="ingredients"
+          compact
+          multiple
+          search
+          selection
+          options={uniqueIngredients}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default connect(null, { addFilter })(Filters);
