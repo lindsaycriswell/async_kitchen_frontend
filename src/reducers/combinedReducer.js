c7a5bd2802b2f40af9146ded7ae66ff7ae67e9c0
github username <github email address>
2018-04-28 13:25:05 -0400

    refactored recipeReducer

--- src/reducers/combinedReducer.js
+++ src/reducers/combinedReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import rootReducer from "./rootReducer";
+import recipeReducer from "./recipeReducer";
 import mealReducer from "./mealReducer";
 
 export default combineReducers({
   meal: mealReducer,
+  recipe: recipeReducer,
   root: rootReducer
 });
